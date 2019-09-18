1ebe74f45a3df73c1728b587cf07ea2b5d4137b9
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-09-18 09:43:15 -0500

    Add page for some tests

--- tests/App.svelte
+++ tests/App.svelte
@@ -1,11 +1,68 @@
 <script>
-  import { Router, Route } from 'svero';
+  import { Router, Route, Link } from 'svero';
+  import { In, Status } from '../src/main';
   import { Pokemon } from './pages/pokemon';
+
+  let promise;
+  let checked;
+  let value;
+  let show;
+
+  function toggle() {
+    show = !show;
+  }
+
+  function load() {
+    promise = new Promise((ok, err) => {
+      setTimeout(() => {
+        if (checked) err(-1);
+        else ok(42)
+      }, value || 1000);
+    });
+  }
 </script>
+
+<style>
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  html, body {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+  }
+</style>
 
 <Router>
   <Route exact path="/">
     <h1>It works!</h1>
+    <Link href="/tests">Go to test-page</Link>
   </Route>
   <Route path="/:name" component={Pokemon} />
+  <Route exact path="/tests">
+    <button on:click={toggle}>Open modal</button>
+    {#if show}
+      <In modal autofocus on:cancel={toggle}>
+        <input type="text" />
+        <button on:click={toggle}>close</button>
+      </In>
+    {/if}
+    <hr />
+    <button on:click={load}>Load promise</button>
+    <label>
+      <input type="checkbox" bind:checked />
+      Throw error?
+    </label>
+    <hr />
+    <label>
+      <input type="number" bind:value />
+      Delay in ms.
+    </label>
+    <Status fixed from={promise} />
+  </Route>
 </Router>
