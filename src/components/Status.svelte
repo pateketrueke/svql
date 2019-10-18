641b692fc8f417903d24dcfa7025c05cd841df2b
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-10-18 03:17:41 -0500

    Wrap given promises and decorate based on its state

--- src/components/Status.svelte
+++ src/components/Status.svelte
@@ -1,6 +1,7 @@
 <script>
   import Failure from './Failure.svelte';
 
+  let status = -1;
   let cssClass = '';
   let fixedClass = '';
   let fixedStyle = '';
@@ -9,13 +10,26 @@
   export let label = 'An error has ocurred';
   export let pending = 'Loading...';
   export let otherwise = 'Done.';
+  export let nodebug = false;
 
   export let fixed = false;
   export let className = '';
   export { cssClass as class };
 
-  // apply fixed-class and/or custom properly...
-  $: fixedClass = [fixed ? 'fixed' : '', className].filter(x => x.length).join(' ');
+  function wrap(promise) {
+    return new Promise((resolve, reject) => {
+      status = -1;
+      promise.then(resolve)
+        .then(result => {
+          status = 1;
+          resolve(result);
+        })
+        .catch(e => {
+          status = 0;
+          reject(e);
+        });
+    });
+  }
 
   // build css-props based on fixed ones...
   $: fixedStyle = fixed && typeof fixed === 'object'
@@ -34,11 +48,34 @@
     bottom: 0;
     left: 0;
   }
+  .pending {
+    background-color: #F7FAFC;
+    color: #6F79E6;
+  }
+  .success {
+    background-color: #CBF4C9;
+    color: #186A4C;
+  }
+  .failed {
+    background-color: #F6E6B9;
+    color: #AC5C2B;
+  }
+  div {
+    padding: 10px;
+  }
 </style>
 
 {#if from}
-  <div style={fixedStyle} class={fixedClass} role="status">
-    {#await from}
+  <div
+    role="status"
+    style={fixedStyle}
+    class={className}
+    class:fixed
+    class:failed={status === 0}
+    class:success={status === 1}
+    class:pending={status === -1}
+  >
+    {#await wrap(from)}
       <slot name="pending">
         <h3>{pending}</h3>
       </slot>
@@ -48,7 +85,7 @@
       </slot>
     {:catch error}
       <slot name="exception" {error}>
-        <Failure {label} {error} />
+        <Failure {nodebug} {label} {error} />
       </slot>
     {/await}
   </div>
